60f631842c19d1d44f1d476355b12adaf4fc9cc7
rizkidinda <56252915+rizkidinda@users.noreply.github.com>
2019-11-28 08:51:33 +0700

    Revert "com"
    This reverts commit 161041baee8df965eb6fd7969c10b781877dfa99, reversing
    changes made to ad6e1cb8d3ffd766d6107de63ceda7bf7d593217.

--- application/models/Info.php
+++ application/models/Info.php
@@ -3,11 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Info extends CI_Model
 {
-<<<<<<< HEAD
-  private $_table = "info_kesehatan";
-=======
   private $_table = "Info";
->>>>>>> ca20538f60eeb485cb624996cd9d50ae5fdf6fe0
 
 
   public function __construct()
@@ -17,15 +13,7 @@ class Info extends CI_Model
 
   public function getAll()
   {
-<<<<<<< HEAD
     return $this->db->get_where($this->_table,array('createBy' => $this->session->userdata('user_userID'), 'status' => 1))->result();
-=======
-<<<<<<< HEAD
-    return $this->db->get($this->_table)->result();
-=======
-    return $this->db->get_where($this->_table,array('createBy' => $this->session->userdata('user_username'), 'status' => 1))->result();
->>>>>>> ca20538f60eeb485cb624996cd9d50ae5fdf6fe0
->>>>>>> 2175251ba3cbbe6f565e37d2184f1c9d61e595fc
   }
 
   public function getByID($id)
@@ -37,16 +25,6 @@ class Info extends CI_Model
 
   public function save()
   {
-<<<<<<< HEAD
-    $dateNow = date("Y-m-d");
-    $post = $this->input->post();
-		$this->Judul = $post["Judul"];
-		$this->Foto = $post["Foto"];
-		$this->Deskripsi = $post["Deskripsi"];
-	$this->status = 1;
-    $this->createby = 1;
-    $this->createDate = $dateNow;
-=======
     $dateNow = date("Y-m-d H:i:s");
     $post = $this->input->post();
 		$this->Judul = $post["judul"];
@@ -57,7 +35,6 @@ class Info extends CI_Model
     $this->createby = $this->session->userdata('user_userID');
     $this->createDate = $dateNow;
     $this->status = 1;
->>>>>>> ca20538f60eeb485cb624996cd9d50ae5fdf6fe0
 		return $this->db->insert($this->_table,$this);
   }
 
@@ -65,16 +42,6 @@ class Info extends CI_Model
   {
     $dateNow = date("Y-m-d");
     $post = $this->input->post();
-<<<<<<< HEAD
-    $this->IDInfo = $post["IDInfo"];
-		$this->Judul = $post["Judul"];
-		$this->Foto = $post["Foto"];
-		$this->Deskripsi = $post["Deskripsi"];
-    $this->status = 1;
-    $this->modifiedby = 1;
-    $this->modifiedDate = $dateNow;
-		return $this->db->update($this->_table,$this,array('IDInfo'=>$post['IDInfo']));
-=======
     $this->IDRole = $post["IDRole"];
     $this->Deskripsi = $post["Deskripsi"];
     $this->gambar = $post["gambar"];
@@ -82,17 +49,11 @@ class Info extends CI_Model
     $this->modifiedby = 1;
     $this->modifiedDate = $dateNow;
 		return $this->db->update($this->_table,$this,array('IDRole'=>$post['IDRole']));
->>>>>>> ca20538f60eeb485cb624996cd9d50ae5fdf6fe0
   }
 
   public function delete($IDInfo)
   {
     $this->status = 0;
-<<<<<<< HEAD
-    $this->modifiedBy = 1;
-    $this->modifiedDate = date("Y-m-d");
-    return $this->db->update($this->_table,array('IDInfo'=>$IDInfo));
-=======
     $this->CreateDate = date("Y-m-d H:i:s");
     return $this->db->update($this->_table,$this,array('IDInfo'=>$IDInfo));
   }
@@ -114,7 +75,6 @@ class Info extends CI_Model
     print_r($this->upload->display_errors());
 
     //return "default.jpg";
->>>>>>> ca20538f60eeb485cb624996cd9d50ae5fdf6fe0
   }
 
 }
